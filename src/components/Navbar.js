725e2d7bd3391ce5e7e1088ee010ad08f40bb672
DadaLV <vynohradova.l.a@gmail.com>
2022-07-24 20:25:11 +0300

    Featured: add css style

---
+++ src/components/Navbar.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import "../styles/navbar.css"
+
+function Navbar() {
+  return (
+    <div className = "navbar">
+
+      <div className='toggleButton'>
+        <button></button>
+        <div className='links'>
+        <Link to="/">Home</Link>
+        <Link to="/projects">Projects</Link>
+        <Link to="/experience">Experience</Link>
+
+        </div>
+      </div>
+       
+    </div>
+  );
+}
+
+export default Navbar; 
