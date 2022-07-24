f523302626b61f7da46478ea8e9cd23154e4bb08
DadaLV <vynohradova.l.a@gmail.com>
2022-07-25 01:20:01 +0300

    Featered: add Experience page

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,23 +1,33 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import "../styles/navbar.css"
+import React, {useEffect, useState} from "react";
+import { Link, useLocation } from "react-router-dom";
+import "../styles/navbar.css";
+import ReorderIcon from "@material-ui/icons/Reorder";
 
 function Navbar() {
-  return (
-    <div className = "navbar">
+  const [expandNavbar, setExpandNavbar] = useState(false);
+  const location = useLocation();
 
-      <div className='toggleButton'>
-        <button></button>
-        <div className='links'>
+  useEffect(() => {
+    setExpandNavbar(false);}, [location]);
+
+  return (
+    <div className="navbar" id={expandNavbar ? "open" : "close"}>
+      <div className="toggleButton">
+        <button
+          onClick={() => {
+            setExpandNavbar((prev) => !prev);
+          }}
+        >
+          <ReorderIcon />
+        </button>
+      </div>
+      <div className="links">
         <Link to="/">Home</Link>
         <Link to="/projects">Projects</Link>
         <Link to="/experience">Experience</Link>
-
-        </div>
       </div>
-       
     </div>
   );
 }
 
-export default Navbar; 
+export default Navbar;
--- src/pages/Experience.js
+++ src/pages/Experience.js
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from "react";
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+import "react-vertical-timeline-component/style.min.css";
+import SchoolIcon from "@material-ui/icons/School";
+import WorkIcon from "@material-ui/icons/Work";
 
 const Experience = () => {
-  return (
-    <div>Experience</div>
-  )
-}
+  return <div className="experience">
+  <VerticalTimeline lineColor="#3e497a">
+    <VerticalTimelineElement className="vertical-timeline-element--education" date="2022-2022" iconStyle={{background: "#3e497a", color: "#fff"}} icon={<SchoolIcon/>}>
+      <h3 className="vertical-timeline-element-title">First Courses</h3>
+      <p>Certificate of</p>
+    </VerticalTimelineElement>
+    <VerticalTimelineElement className="vertical-timeline-element--education" date="2022-2022" iconStyle={{background: "#3e497a", color: "#fff"}} icon={<WorkIcon/>}>
+      <h3 className="vertical-timeline-element-title">First Courses</h3>
+      <p>Certificate of</p>
+    </VerticalTimelineElement>
+    <VerticalTimelineElement className="vertical-timeline-element--education" date="2022-2022" iconStyle={{background: "#e9d35d", color: "#fff"}} icon={<SchoolIcon/>}>
+      <h3 className="vertical-timeline-element-title">First Courses</h3>
+      <p>Certificate of</p>
+    </VerticalTimelineElement>
+  </VerticalTimeline>
+  </div>;
+};
 
-export default Experience
+export default Experience;
