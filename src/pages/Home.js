927a696129e5041c843238abe9d16118940c7864
DadaLV <vynohradova.l.a@gmail.com>
2022-07-25 03:25:16 +0300

    Fix: fix bag

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import GitHubIcon from '@material-ui/icons//GitHub';
+import GitHubIcon from '@material-ui/icons/GitHub';
 import LinkedInIcon from "@material-ui/icons/LinkedIn";
 import "../styles/home.css"
 
